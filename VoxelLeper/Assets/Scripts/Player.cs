13cdc39982a98bd5a46ce4c48d35ded6820cb27f
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-12-01 22:10:55 +1030

    displays health on screen

--- VoxelLeper/Assets/Scripts/Player.cs
+++ VoxelLeper/Assets/Scripts/Player.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
     public int health = 64;
+    public Text healthTextUI;
+
+    private void Start()
+    {
+        // maybe put this somewhere better to manage UI updates
+        DisplayHealth();
+    }
 
     public void ReduceHealth(int Amount)
     {
@@ -18,13 +26,27 @@ public class Player : MonoBehaviour {
             // decrease in size
             transform.localScale -= new Vector3(0.015f, 0.015f, 0.015f);
         }
-            
 
-        
+        DisplayHealth();
+
     }
 
     public bool IsAlive()
     {
         return health > 0;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Killzone")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void DisplayHealth()
+    {
+        healthTextUI.text = "Health: " + health.ToString();
     }
 }
