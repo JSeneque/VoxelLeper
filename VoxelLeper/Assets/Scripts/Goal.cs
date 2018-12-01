43373ac2237328d62ef10d82bc88fc3a9c0a17b4
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-12-02 00:19:59 +1030

    Add small cube idle animation and finish screen

---
+++ VoxelLeper/Assets/Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    public string loadNextLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<SceneTransitions>().LoadScene(loadNextLevel);
+        }
+    }
+}
